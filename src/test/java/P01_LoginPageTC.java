3d72bc9ade5199883131747fd5aa7b2dbab2702c
Ahmed Hassan <eng.ahmedhasan11@gmail.com>
2025-07-25 15:46:39 +0300

    Add files via upload

---
+++ src/test/java/P01_LoginPageTC.java
@@ -0,0 +1,90 @@
+import ExtentReports.ExtentReport;
+import TestComponent.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.P01_LoginPage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class P01_LoginPageTC extends BaseTest {
+    ExtentReport extent =new ExtentReport();
+    P01_LoginPage LoginPage;
+    @BeforeClass
+    public void setup() {
+        LoginPage = new P01_LoginPage(driver);
+    }
+
+
+    @BeforeMethod
+    public void goToLogin() {
+        LoginPage.goTo();
+    }
+
+@DataProvider
+    public Object[][] getValidData() throws IOException {
+
+        List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+getJsonPath("loginValidData"));
+
+
+    return data.stream()
+            .map(entry -> new Object[]{entry})
+            .toArray(Object[][]::new);
+    }
+    @DataProvider
+    public Object[][] getInvalidData() throws IOException {
+
+        List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+getJsonPath("loginInvalidData"));
+
+
+        return data.stream()
+                .map(entry -> new Object[]{entry})
+                .toArray(Object[][]::new);
+    }
+
+    @Test(priority = 1, groups = {"login","smoke"})
+    public void testScreenshot() {
+        attachScreenshot(driver);
+    }
+    @Test(dataProvider="getValidData" ,priority = 1, groups = {"login","smoke"})
+    public void validUserNameAndPassword(HashMap<String,String> input)  {
+        //LoginPage.goTo();
+       // ExtentTest test = extent.createTest("init");
+        LoginPage.login(input.get("user_Name"),input.get("password") );
+        LoginPage.sub();
+        Assert.assertEquals(driver.getCurrentUrl(),input.get("afterLoginUrl"));
+    }
+
+    @Test(dataProvider = "getValidData",priority = 2 ,groups = {"login","regression"})
+    public void validUserNameAndemptyPassword(HashMap<String,String> input) {
+        //LoginPage.goTo();
+        LoginPage.login(input.get("user_Name"), "");
+        LoginPage.sub();
+        Assert.assertEquals(driver.getCurrentUrl(),input.get("LoginUrl"));
+        String errorMessage = LoginPage.getErrorMessage();
+        Assert.assertEquals(errorMessage,"Epic sadface: Password is required");
+
+
+    }
+
+    @Test(dataProvider = "getValidData",priority = 3,groups = {"login","regression"})
+    public void validPasswordAndemptyUsername(HashMap<String,String> input) {
+        //LoginPage.goTo();
+        LoginPage.login("", input.get("password"));
+        LoginPage.sub();
+     String errorMessage = LoginPage.getErrorMessage();
+     Assert.assertEquals(errorMessage,"Epic sadface: Username is required");
+    }
+
+    @Test(dataProvider = "getInvalidData",priority = 4,groups = {"login","regression"})
+    public void vlidationLockeUser(HashMap<String,String> input) {
+
+        LoginPage.login(input.get("lockedName"), input.get("password"));
+        LoginPage.sub();
+    }
+
+
+
+
+}
---
+++ src/test/java/P02_ProductsPageTC.java
@@ -0,0 +1,116 @@
+import TestComponent.BaseTest;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.P01_LoginPage;
+import pages.P02_ProductsPage;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class P02_ProductsPageTC extends BaseTest {
+
+
+    P02_ProductsPage productsPage;
+    List<WebElement> products;
+
+    @DataProvider
+    public Object[][] getProductsData() throws IOException {
+
+        List<HashMap<String, String>> data = getJsonDataToMap(System.getProperty("user.dir") + getJsonPath("productsData"));
+
+
+        return data.stream()
+                .map(entry -> new Object[]{entry})
+                .toArray(Object[][]::new);
+    }
+
+
+    @BeforeClass
+    public void setup() {
+        productsPage = new P02_ProductsPage(driver);
+        productsPage.goTo();
+
+    }
+
+
+    @Test(dataProvider = "getProductsData", priority = 1, groups = {"products", "smoke", "regression"})
+    public void addToCart(HashMap<String, String> input) {
+
+        if (input.get("action").equals("add")) {
+            productsPage.addToCart(input.get("productName"));
+
+        }
+    }
+
+    @Test( priority = 2, groups = {"products","regression"})
+    public void verifyRemoveButtonDisappearsAfterReset()  {
+
+      productsPage.clickBurgerIcon();
+        productsPage.clickresetAppState();
+        products = productsPage.getProductsList();
+        for (WebElement prod : products) {
+            Assert.assertFalse(productsPage.isARemoveButtonVisible(prod));
+
+        }
+
+    }
+
+    @Test(dataProvider = "getProductsData", priority = 3, groups = {"products","regression"})
+    public void addToCartAfterResset(HashMap<String, String> input) {
+        productsPage.goTo();
+        if (input.get("action").equals("add")) {
+            productsPage.addToCart(input.get("productName"));
+
+        }
+    }
+
+    @Test(priority = 4, groups = {"products","regression"})
+    public void verifySortOrderAToZ() {
+        productsPage.choosefilter("AToZ");
+        products = productsPage.getProductsList();
+
+        for (int i = 0; i < products.size() - 1; i++) {
+            Assert.assertTrue(productsPage.getProductName(products.get(i)).compareTo(productsPage.getProductName(products.get(i + 1))) <= 0);
+        }
+
+    }
+
+    @Test(priority = 5, groups = {"products", "regression"})
+    public void verifySortOrderZToA() {
+        productsPage.choosefilter("ZToA");
+        products = productsPage.getProductsList();
+
+        for (int i = 0; i < products.size() - 1; i++) {
+            Assert.assertTrue(productsPage.getProductName(products.get(i)).compareToIgnoreCase(productsPage.getProductName(products.get(i + 1))) >= 0);
+        }
+
+
+    }
+
+    @Test(priority = 6, groups = {"products", "regression"})
+    public void verifySortOrderLowToHigh() {
+        productsPage.choosefilter("LowToHigh");
+        products = productsPage.getProductsList();
+        for (int i = 0; i < products.size() - 1; i++) {
+            Assert.assertTrue(productsPage.getProductPrice(products.get(i)) <= (productsPage.getProductPrice(products.get(i + 1))));
+        }
+
+    }
+
+    @Test(priority = 7, groups = {"products", "regression"})
+    public void verifySortOrderHighToLow() {
+        productsPage.choosefilter("HighToLow");
+        products = productsPage.getProductsList();
+        for (int i = 0; i < products.size() - 1; i++) {
+            Assert.assertTrue(productsPage.getProductPrice(products.get(i)) >= (productsPage.getProductPrice(products.get(i + 1))));
+        }
+
+    }
+
+
+}
